4b052e59de21f2ad7230b19b6b187ed8834344a3
Taylor Riley Du <taylorsriley@gmail.com>
2020-09-29 12:25:23 -0400

    Created components for columns, database file, and installed react-beautiful-dnd

---
+++ client/containers/StatusColumn.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+
+// Droppable containers for application cards
+
+const StatusColumn = (props) => {
+  return <div></div>;
+};
+
+export default StatusColumn;
---
+++ server/database.js
@@ -0,0 +1,24 @@
+// URI: postgres://uquhkhrl:x6ledfJxj4HSiDzjuw8ySndw-pu9Gd6f@lallah.db.elephantsql.com:5432/uquhkhrl
+
+/* LinkedIn API
+{
+    "elements":[
+       {
+          "job":{
+             "jobDescriptionSnippet":"This is a sample job description snippet.",
+             "companyName":"LinkedIn",
+             "locationDescription":"San Francisco Bay Area",
+             "id":3580154,
+             "title":"Software Engineer"
+          }
+       }
+    ],
+    "paging":{
+       "count":10,
+       "start":0,
+       "links":[
+ 
+       ]
+    }
+ }
+*/
