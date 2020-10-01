9020b7ffdf5e9610a55c35a3b0f70a4553c97caf
Nick Kruckenberg <nkruckenberg@gmail.com>
2020-10-01 15:58:40 -0400

    Restructured queries and routes to post new application to database and fetch all applications for the logged-in user.

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import appController from './controllers/appController';
+const express = require('express');
+const appController = require('./controllers/appController');
 
 const router = express.Router();
 
